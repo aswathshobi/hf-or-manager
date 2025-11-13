3537dc9b86910cdae0bd87a8f5b952265c74fc0e
Aswa <aswath251999@gmail.com>
2025-11-13 19:07:14 +0530

    Update application.js

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,7 +4,7 @@ import ENV from 'org-manager/config/environment';
 export default class ApplicationAdapter extends RESTAdapter {
   get host() {
     // Use environment variable in production, localhost in development
-    return ENV.apiHost || 'http://localhost:3000';
+    return ENV.apiHost || 'https://org-s.onrender.com';
   }
   
   namespace = '';
